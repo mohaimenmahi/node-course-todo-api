361a97fc73fccc593d6be9db403f2f7b42f30b00
Mohaimen Chowdhury Mahi <mmc.dp11@gmail.com>
2017-05-17 21:41:50 +0600

    push delete

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,26 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err) {
+    return console.log('Unable to connect MongoDB server.');
+  }
+  console.log('Connected to MongoDB server.');
+
+  // deleteMany
+  db.collection('User').deleteMany({name: 'Mahi'}).then((res) => {
+    console.log(res.result);
+  });
+
+  //deleteOne
+  // db.collection('User').deleteOne({name: 'Mahi'}).then((res) => {
+  //   console.log(res.result);
+  // });
+
+  //findOneAndDelete
+  // db.collection('User').findOneAndDelete({name: 'Mahi'}).then((res) => {
+  //   console.log(res);
+  // });
+
+  //db.close();
+})
