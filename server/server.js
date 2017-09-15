84be8d1442e6f3ffa9d57957d274898066608c55
Mohaimen Chowdhury Mahi <mmc.dp11@gmail.com>
2017-09-16 00:46:28 +0600

    GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -27,6 +29,21 @@ app.get('/todos', (req,res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todos) => {
+    if(!todos) {
+      return res.status(404).send();
+    }
+    res.status(400).send(todos);
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
