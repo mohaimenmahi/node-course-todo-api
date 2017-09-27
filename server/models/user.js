93fc28d4b7d133713227e25bb0a175a5f193dc08
Mohaimen Chowdhury Mahi <mmc.dp11@gmail.com>
2017-09-27 22:39:35 +0600

    adding login

--- server/models/user.js
+++ server/models/user.js
@@ -33,7 +33,7 @@ var UserSchema = new mongoose.Schema({
   }]
 });
 
-UserSchema.methods.toJSON = function () { // only email and id will be visible. using _.pick()
+UserSchema.methods.toJSON = function () { // only email and id will be visible. using _.pick() ***check again
   var user = this;
   var userObject = user.toObject();
 
@@ -73,6 +73,26 @@ UserSchema.statics.findByToken = function (token) {
     '_id': decoded._id,
     'tokens.token': token,
     'tokens.access': 'auth'
+  });
+};
+
+// logging in
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if(!user) {
+      return Promise.reject();
+    }
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if(res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
   });
 };
 
--- server/server.js
+++ server/server.js
@@ -104,12 +104,23 @@ app.post('/users', (req,res) => {
 // get request below:
 // private routs and auth middlewares (middleware: like glue between os and ui)
 // express routs into private routs
-
 // adding middleware function to make the routs private
 // go to middleware/authenticate
 
 app.get('/users/me', authenticate, (req,res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req,res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
